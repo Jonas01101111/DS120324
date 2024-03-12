2b7f0bd582b2273673bd6483f246a590fcfaa0dc
Usuario <Usuario@CPD02-M08>
2024-03-12 11:45:13 -0300

    Adicionar arquivos de projeto.

---
+++ IMC/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira seu peso em KG:");
+            double peso = double.Parse(Console.ReadLine());
+            Console.WriteLine("Insira sua altura em Metros:");
+            double altura = double.Parse(Console.ReadLine());
+
+            double imc = peso / (altura * altura);
+
+
+
+            if (imc < 16.9)
+            {
+                Console.WriteLine("Muito abaixo do peso");
+            }
+            else if (imc >= 17 && imc <= 18.4)
+            {
+                Console.WriteLine("Abaixo do peso");
+            }
+            else if (imc >= 18.5 && imc <= 24.9)
+            {
+                Console.WriteLine("Peso normal");
+            }
+            else if (imc >= 25 && imc <= 29.9)
+            {
+                Console.WriteLine("Acima do peso");
+            }
+            else if (imc >= 30 && imc <= 34.9)
+            {
+                Console.WriteLine("Obesidade Grau 1");
+            }
+            else if (imc >= 35 && imc <= 40)
+            {
+                Console.WriteLine("Obesidade Grau 2");
+            }
+            else if (imc > 40)
+            {
+                Console.WriteLine("Obesidade Grau 3");
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Presenca/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presenca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira a média do aluno:");
+                int media = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insira a presença do aluno:");
+                int presenca = int.Parse(Console.ReadLine());
+
+            if (media >= 7 && presenca >=75)
+            {
+                Console.WriteLine("Aprovado");
+            }
+            else if (media < 4 && presenca < 75)
+            {
+                Console.WriteLine("Retido");
+            }
+            else
+            {
+                Console.WriteLine("Recuperação");
+            }
+           
+            Console.ReadKey();
+        }
+    }
+}

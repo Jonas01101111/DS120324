e2b9b81161cc7759677e74940b08e2dc9e68160f
Usuario <Usuario@CPD02-M08>
2024-03-12 12:17:52 -0300

    :D

---
+++ DescansoMerecido/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DescansoMerecido
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira sua idade:");
+            int idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insira seu tempo de contribuição:");
+            int trabalho= int.Parse(Console.ReadLine());
+
+            
+
+            if ((idade >= 65) || (trabalho >= 30) ||(idade >= 60 && trabalho >= 25))
+            { 
+                Console.WriteLine("Seu descanso é merecido");
+            }
+            else 
+            {
+                int faltai = 60 - idade;
+                int faltat = 25 - trabalho;
+                Console.WriteLine("Sua hora não chegou, te faltam "+ faltai + " anos de vida e " + faltat + " anos de trabalho para o descanso merecido");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
